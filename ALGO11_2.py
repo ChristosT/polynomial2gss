1ca07a9e40624db0ccf7963707634b58098ac606
Christos <tsolakic@zoho.com>
2014-02-20 16:54:54 +0200

    remove wrong assumption which treated List_P matrices as if it was square matrices, also fix the check function

--- ALGO11_2.py
+++ ALGO11_2.py
@@ -4,7 +4,6 @@ import numpy as np
 import matrix_coefficients as mc
 s=symbols('s')
 def ALGO11(As,Bs,Cs,Ds,do_test):
-
     #=======================STEP 1======================================
     r=As.rows
     m=Bs.cols
@@ -15,6 +14,7 @@ def ALGO11(As,Bs,Cs,Ds,do_test):
     List_P.reverse()
     q=len(List_P)
     k=List_P[0].rows
+    l=List_P[0].cols
     Zero_Mat=zeros(List_P[0].rows,List_P[0].cols)  #list of zeroes
     #==================END==STEP 1======================================
     
@@ -48,23 +48,20 @@ def ALGO11(As,Bs,Cs,Ds,do_test):
     
     PE=Matrix(np.mat(PiE)[np.ix_(I,J)])
     PA=Matrix(np.mat(PiA)[np.ix_(I,J)])
-    PB=Matrix(np.mat(PiB)[np.ix_(I,range(k))])
+    PB=Matrix(np.mat(PiB)[np.ix_(I,range(l))])
     PC=Matrix(np.mat(PiC)[np.ix_(range(k),J)])
     #===================END=STEP 3======================================
     #=======================STEP 4======================================
     Lambda=r+rE +p +m
-    
-    E=SparseMatrix(BlockMatrix([[List_P[1],PC,zeros(PC.rows,Lambda-k-PC.cols)],
-    [PB,PA,zeros(PA.rows,Lambda-PB.cols -PA.cols)],
-    [zeros(Lambda-k-PB.rows,List_P[1].cols),zeros(Lambda-k-PB.rows,PC.cols),zeros(Lambda-k-PB.rows,Lambda-k-len(J))]]))
-    
-    A=SparseMatrix(BlockMatrix([[-List_P[0],zeros(k,PE.cols),zeros(k-p,Lambda-k-PE.cols).col_join(-eye(p))],
-    [zeros(PE.rows,k),PE,zeros(PE.rows,p)],
-    [zeros(m,k-m).row_join(eye(m)),zeros(m,PE.cols),zeros(m,p)]]))
+    E=BlockMatrix([[List_P[1],PC],[PB,PA],[zeros(Lambda-k-PB.rows,PB.cols),zeros(Lambda-k-PB.rows,PA.cols)]]).as_mutable()
+    E=E.row_join(zeros(Lambda,Lambda-l-PC.cols))
+    A=BlockMatrix([[-List_P[0],zeros(k,PE.cols),zeros(k-p,Lambda-l-PE.cols).col_join(-eye(p))],
+    [zeros(PE.rows,l),PE,zeros(PE.rows,p)],
+    [zeros(m,l-m).row_join(eye(m)),zeros(m,PE.cols),zeros(m,p)]]).as_mutable()
     
     
-    B=SparseMatrix(BlockMatrix([[zeros(r+p+rE,m)],[eye(m)]]))
-    C=SparseMatrix(BlockMatrix([[zeros(p,r+m+rE),eye(p)]]))
+    B=BlockMatrix([[zeros(r+p+rE,m)],[eye(m)]]).as_mutable()
+    C=BlockMatrix([[zeros(p,r+m+rE),eye(p)]]).as_mutable()
     D=zeros(p,m)
     #===============END==STEP 4=========================================
 
@@ -72,8 +69,6 @@ def ALGO11(As,Bs,Cs,Ds,do_test):
         test_result=test(rE,r,p,m,PE,PA,PC,As,Bs,Cs,Ds,E,A,B,C,D)
     else:
         test_result= 'not done' 
-    
-    D=zeros(p,m)
     return E,A,B,C,D,test_result
     
 def test(rE,r,p,m,PE,PA,PC,As,Bs,Cs,Ds,E,A,B,C,D):
@@ -82,6 +77,7 @@ def test(rE,r,p,m,PE,PA,PC,As,Bs,Cs,Ds,E,A,B,C,D):
     D1Block4=BlockMatrix([[zeros(r,p)],[eye(p)]]).as_mutable()
     D1=BlockMatrix([[eye(r+p),D1Block2,D1Block3,D1Block4]]).as_mutable()
     D2=BlockMatrix(2,2,[s*E-A,B,-C,D]).as_mutable()
+
     D3=BlockMatrix(2,2,[As,Bs,-Cs,Ds]).as_mutable()
     D4=BlockMatrix([[eye(r),zeros(r,rE+2*p),zeros(r,m)],
                     [zeros(m,r),zeros(m,rE+2*p),eye(m)]]).as_mutable()
